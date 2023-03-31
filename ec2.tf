19bb96507707c34706543e16bbed546850279c37
sunilrch <56264447+sunilrch@users.noreply.github.com>
2023-03-31 15:12:28 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
-  ami                    = "ami-08c40ec9ead489470"
-  instance_type          = "t2.medium"
+  ami                    = "ami-0376ec8eacdf70aae"
+  instance_type          = "t2.micro"
   key_name               = "demo-keypair"
   vpc_security_group_ids = [aws_security_group.allow_sg.id]
   
