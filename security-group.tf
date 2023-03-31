b1c3cf970b8cc75f6278138912ac8e9d37443256
sunilrch <56264447+sunilrch@users.noreply.github.com>
2023-03-31 15:01:54 +0530

    Create security-group.tf

---
+++ security-group.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "allow_sg" {
+  name        = "Terraform-SG"
+  description = "Allow tls inbound traffic"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["157.35.3.5/32"]
+  }
+  
+   ingress {
+    description      = "TLS from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["157.35.3.5/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Terraform-SG"
+  }
+}
